c8fd3f16b43f782d326aed3abf699e620c6264a8
Srthk-08 <sarthaknamdev2002@gmail.com>
2025-12-31 17:08:30 +0530

    push

--- src/services/socketService.js
+++ src/services/socketService.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+const API_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api';
 const SOCKET_URL = API_URL.replace(/\/api\/?$/, '');
 
 class SocketService {
